f6f304919d0ee0f4a21b50da56efe5bb70975e19
Nathan Luo <naipton@gmail.com>
2015-10-21 00:14:50 -0400

    update styling and jquery for dynamic div height

--- jsmodules/blog_menu.js
+++ jsmodules/blog_menu.js
@@ -3,14 +3,11 @@ $(document).ready(function(){
     var innerItem = $(this).children();
     innerItem.slideToggle('fast');
     console.log($(this).css("height"));
-    if ($(this).css("height")=="112px"){
-      $(this).animate({height:"7em"}, 200);
-    }
-    else{
-      $(this).animate({height:"4em"}, 200);
-    }
-
+    // if ($(this).css("height")=="105px"){
+    //   $(this).animate({height:"9em"}, 200);
+    // }
+    // else{
+    //   $(this).animate({height:"5em"}, 200);
+    // }
   })
-
-
 })
